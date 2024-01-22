06ec7050136ce744e9cfecb616a842f4fe616d62
vladimir <tarskij-vladimir@yandex.ru>
2024-01-22 19:13:14 +0300

    added changes

--- DZ.py
+++ DZ.py
@@ -1,12 +1,48 @@
-import pandas as pd
 
+
+
+
+import pandas as pd 
+import numpy as np 
+import random
+ 
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI': lst})
+print(data)
+ 
+#==================================================#
+data['tmp'] = 1
+data.set_index([data.index, 'whoAmI'], inplace=True)
+data = data.unstack(level=-1, fill_value = 0).astype(int)
+data.columns = data.columns.droplevel()
+data.columns.name = None
+print(data)
 
-# Преобразование в one hot вид
-one_hot = pd.get_dummies(data['whoAmI'])
-data = data.drop('whoAmI', axis=1)
-data = data.join(one_hot)
-data.head()
+
+# import random
+# import pandas as pd
+# lst = ['robot'] * 10
+# lst += ['human'] * 10
+# random.shuffle(lst)
+# print(lst)
+# print()
+# data = pd.DataFrame({'whoAmI':lst})
+# data.loc[data['whoAmI'] == 'robot', 'robot_group'] = '1'
+# data.loc[data['whoAmI'] != 'robot', 'robot_group'] = '0'
+# data.loc[data['whoAmI'] == 'human', 'human_group'] = '1'
+# data.loc[data['whoAmI'] != 'human', 'human_group'] = '0'
+
+# data.head(n=20) 
+
+
+# lst = ['robot'] * 10
+# lst += ['human'] * 10
+# col1 = ['robot']
+# col2 = ['human']
+# random.shuffle(lst)
+# data = pd.DataFrame({'whoAmI': lst, 'robot':col1, 'human':col2})
+# data
+
+
